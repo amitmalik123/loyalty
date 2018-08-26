655e619b6c4c6e40b6a7b426b7ef1b037787a017
DSG <DSG@DR_MANOJ>
2018-08-26 21:43:57 +0530

    uploading complete project

---
+++ loyalty.lux/src/test/java/loyalty/lux/demoTest.java
@@ -0,0 +1,28 @@
+package loyalty.lux;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	
+	@Test
+	public void sum(){
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+	
+	@Test
+	public void min(){
+		int a=10;
+		int b=20;
+		System.out.println(b-a);
+	}
+	
+	@Test
+	public void div(){
+		int a=10;
+		int b=20;
+		System.out.println(b/a);
+	}
+
+}
